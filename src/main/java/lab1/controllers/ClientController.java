b7c63104b6625bb72902a74339251df5ca0d5da5
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-09-24 12:58:51 +0300

    add update function for clients

--- src/main/java/lab1/controllers/ClientController.java
+++ src/main/java/lab1/controllers/ClientController.java
@@ -1,8 +1,6 @@
 package lab1.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +8,32 @@ import java.util.Map;
 @RestController
 public class ClientController {
 
+    private final Map<Integer, String> clients = new HashMap<>();
+
+    public ClientController() {
+        clients.put(1, "Client 1");
+        clients.put(2, "Client 2");
+    }
+
     @GetMapping("/clients/{clientId}")
     public Map<String, String> getClient(@PathVariable int clientId) {
         Map<String, String> response = new HashMap<>();
         response.put("id", String.valueOf(clientId));
         response.put("client", clientId + " client");
+        return response;
+    }
+
+    @PutMapping("/update/{clientId}")
+    public Map<String, String> updateClient(@PathVariable int clientId, @RequestParam String name) {
+        Map<String, String> response = new HashMap<>();
+        if (clients.containsKey(clientId)) {
+            clients.put(clientId, name);
+            response.put("success", "Client updated successfully");
+            response.put("id", String.valueOf(clientId));
+            response.put("client", name);
+        } else {
+            response.put("error", "Client not found");
+        }
         return response;
     }
 }
